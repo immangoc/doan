27b153203d716a93394711984d660a356b8528f3
Ma Thế Ngọc <mangoc@Mac.lan>
2026-04-18 05:28:00 +0700

    first commit

---
+++ warehouse-service/src/main/java/com/anhnht/warehouse/service/common/config/PayOSConfig.java
@@ -0,0 +1,28 @@
+package com.anhnht.warehouse.service.common.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class PayOSConfig {
+
+    @Value("${payos.client-id}")
+    private String clientId;
+
+    @Value("${payos.api-key}")
+    private String apiKey;
+
+    @Value("${payos.checksum-key}")
+    private String checksumKey;
+
+    @Value("${payos.base-url}")
+    private String baseUrl;
+
+    @Value("${payos.return-url}")
+    private String returnUrl;
+
+    @Value("${payos.cancel-url}")
+    private String cancelUrl;
+}
